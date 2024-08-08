1b9b1ff4cfb3806f5e22d6a3cc3390d1337b2bce
shawn <chengjun_yo@163.com>
2024-08-08 19:30:01 +0800

    remove comment

--- vn007+/boot/daemon.sh
+++ vn007+/boot/daemon.sh
@@ -7,4 +7,3 @@ export V2RAYA_V2RAY_BIN=/home/root/.local/share/v2ray/x2ray
 export V2RAYA_V2RAY_ASSETSDIR=/home/root/.local/share/v2ray/
 
 /sbin/start-stop-daemon --start --background --exec /home/root/.local/share/v2ray/boot/v2raya.sh
-```
